abe97183c7971119d56f18fe58dc83d083d1d0ba
ohhyesbhi <kamatabhishek14@gmail.com>
2023-10-06 11:42:27 +0530

    added example on parameterConstraints

--- genericsInterface.ts
+++ genericsInterface.ts
@@ -1,3 +1,5 @@
+//                1)
+
 interface genericsI <t , t1>{
     name : t,
     date : t1
@@ -11,3 +13,31 @@ const obj : genericsI<string,number>={
 
 console.log(obj)
 
+//                 2)
+
+interface Logable {
+    log: () => void;
+  }
+
+  function logItems<T>(items: T[]): void {
+    items.forEach(item => item.log());
+  }
+
+// here it accepts that each element of an array should have the log function but actually we can see they don't have so it starts giving an error
+// and when we do .....
+
+interface Logable {
+    log: () => void;
+  }
+
+  function logItems<T extends Logable>(items: T[]): void {
+    items.forEach(item => item.log());
+  }
+
+// now we can see that error has been gone , here we are not going to pass any kind of generic type we are only
+// going to pass the generic type which can extend Loaggable interface now let's see what is going on now we are
+// not saying that all the possible different types can be passed here in the log items but we are saying that 
+// among all the different types only those types which have implemented this Logable interface are going to
+// be supported this is nothing but PARAMETER CONSTRAINTS where we are not going to allow each and every different
+// type of types we are going to constraint it that only the particular type of a type can added as generic in functions
+ 
